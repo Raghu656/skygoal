3ccf6a59677a717b7ad5897dd9c9e47d2f1a4bb1
ragha656 <raghu.nimmana@sweken.com>
2024-04-05 11:24:54 +0530

    add one api

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,15 @@ const router = require("express").Router();
 const { validate } = require("../middlewares/validate");
 const User = require("../models/user");
 
+// give to Bearer Token
 router.get("/getuserdata", validate, async (req, res) => {
   return res.json(req.user);
+});
+
+// this is unauthorized API
+// don't give to Bearer Token
+router.get("/allusersdata", validate, async (req, res) => {
+  res.send(await User.find().sort({ _id: -1 }));
 });
 
 module.exports = router;
